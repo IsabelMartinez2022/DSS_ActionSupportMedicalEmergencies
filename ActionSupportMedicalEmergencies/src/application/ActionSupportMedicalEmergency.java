40d54aa8b657b3c0d5e1fa03b6766b9fea97f7c9
mruedagarcia <m.rueda6@usp.ceu.es>
2024-02-22 19:33:09 +0100

    main

---
+++ ActionSupportMedicalEmergencies/src/application/ActionSupportMedicalEmergency.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+import objects.MedicalEmergency;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ *
+ * @author maria
+ */
+public class ActionSupportMedicalEmergency {
+     public static void main(String[] args) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+
+        execute(kc);
+    }
+
+    public static void execute(KieContainer kc) {
+        KieSession ksession = kc.newKieSession("ATMKS");
+        //MedicalEmergency me1 = new MedicalEmergency("no", "no", "no", );
+    }
+}
